1f3952d78c780fd993c2a4a38a4cf8338dc8f5ed
maxpro <maxim.procopenco@gmail.com>
2023-01-26 16:39:22 +0200

    Rename styles to style

--- src/box.tsx
+++ src/box.tsx
@@ -9,12 +9,12 @@ interface Props<TTag extends keyof React.ReactHTML> extends BoxStyles {
   children?: React.ReactNode | ((props: { isHover: boolean }) => React.ReactNode);
   tag?: TTag;
   props?: TagPropsType<TTag>;
-  styles?: React.ComponentProps<TTag>['style'];
+  style?: React.ComponentProps<TTag>['style'];
   className?: ClassNameUtils.ClassNameType;
 }
 
 export default function Box<TTag extends keyof React.ReactHTML = 'div'>(props: Props<TTag>) {
-  const { tag, children, props: tagProps, className, styles } = props;
+  const { tag, children, props: tagProps, className, style } = props;
 
   const classNames = className ? ClassNameUtils.classNames(className, classes.box) : [classes.box];
   Object.entries(props).forEach(([key, value]) => {
@@ -24,7 +24,7 @@ export default function Box<TTag extends keyof React.ReactHTML = 'div'>(props: P
 
   const boxTag = tag || 'div';
 
-  const finalTagProps = { ...tagProps, style: styles, className: classNames.join(' ') };
+  const finalTagProps = { ...tagProps, style, className: classNames.join(' ') };
 
   const [isHover, setIsHover] = useState(false);
   const needsHoverState = typeof children === 'function';
