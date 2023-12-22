7a6aea0ea51a231b725d99f402e0bae26a48322b
maxpro <maxim.procopenco@gmail.com>
2023-12-22 14:14:34 +0200

    Add deps to tooltip hooks

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -15,41 +15,50 @@ export default function Tooltip(props: Props) {
   const [position, setPosition] = useState<{ top: number; left: number; width?: number } | undefined>();
   const portalContainer = usePortalContainer();
 
-  const observeScroll = useCallback((element: HTMLDivElement, callback: (el: HTMLDivElement) => void) => {
-    const listener = (e: Event) => {
-      if ((e.target as HTMLElement).contains(element)) {
+  const observeScroll = useCallback(
+    (element: HTMLDivElement, callback: (el: HTMLDivElement) => void) => {
+      const listener = (e: Event) => {
+        if ((e.target as HTMLElement).contains(element)) {
+          callback(element);
+        }
+      };
+
+      document.addEventListener('scroll', listener, { capture: true });
+      return () => {
+        document.removeEventListener('scroll', listener, { capture: true });
+      };
+    },
+    [position],
+  );
+
+  const observeResize = useCallback(
+    (element: HTMLDivElement, callback: (el: HTMLDivElement) => void) => {
+      const listener = (e: Event) => {
         callback(element);
+      };
+
+      window.addEventListener('resize', listener, { capture: true });
+      return () => {
+        window.removeEventListener('resize', listener, { capture: true });
+      };
+    },
+    [position],
+  );
+
+  const positionHandler = useCallback(
+    (el: HTMLDivElement) => {
+      const rect = el.getBoundingClientRect();
+
+      const top = Math.round((rect.top + window.scrollY) * positionDigitsAfterComma) / positionDigitsAfterComma;
+      const left = Math.round((rect.left + window.scrollX) * positionDigitsAfterComma) / positionDigitsAfterComma;
+
+      if (position?.top !== top || position?.left !== left) {
+        onPositionChange?.({ top, left });
+        setPosition({ top, left, width: rect.width > 0 ? rect.width : undefined });
       }
-    };
-
-    document.addEventListener('scroll', listener, { capture: true });
-    return () => {
-      document.removeEventListener('scroll', listener, { capture: true });
-    };
-  }, []);
-
-  const observeResize = useCallback((element: HTMLDivElement, callback: (el: HTMLDivElement) => void) => {
-    const listener = (e: Event) => {
-      callback(element);
-    };
-
-    window.addEventListener('resize', listener, { capture: true });
-    return () => {
-      window.removeEventListener('resize', listener, { capture: true });
-    };
-  }, []);
-
-  const positionHandler = useCallback((el: HTMLDivElement) => {
-    const rect = el.getBoundingClientRect();
-
-    const top = Math.round((rect.top + window.scrollY) * positionDigitsAfterComma) / positionDigitsAfterComma;
-    const left = Math.round((rect.left + window.scrollX) * positionDigitsAfterComma) / positionDigitsAfterComma;
-
-    if (position?.top !== top || position?.left !== left) {
-      onPositionChange?.({ top, left });
-      setPosition({ top, left, width: rect.width > 0 ? rect.width : undefined });
-    }
-  }, []);
+    },
+    [position],
+  );
 
   useLayoutEffect(() => {
     if (ref.current) {
@@ -62,7 +71,7 @@ export default function Tooltip(props: Props) {
         resizeHandlerDispose();
       };
     }
-  }, []);
+  }, [position]);
 
   return (
     <>
