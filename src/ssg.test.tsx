f453dfde82927df9dbe876f61898910a9de95340
maxpro <maxim.procopenco@gmail.com>
2024-06-18 15:08:07 +0300

    Add test

--- src/ssg.test.tsx
+++ src/ssg.test.tsx
@@ -21,7 +21,7 @@ describe('SSG', () => {
 
     const result = renderToStaticMarkup(el);
 
-    expect(result.html).toEqual('<div class="_box hoverGroupparent"><div class="_box displayHoverparentgrid">test</div></div>');
+    expect(result.html).toEqual('<div class="_box hoverGroupparent"><div class="_box displayhoverparentgrid">test</div></div>');
   });
 
   it('generates static html with hoverGroup2', () => {
@@ -55,7 +55,7 @@ body{margin: 0;line-height: var(--lineHeight);font-size: var(--fontSize);}
 a,ul{all: unset;}
 ._box{display: block;border: 0 solid var(--borderColor);outline: 0px solid var(--outlineColor);margin: 0;padding: 0;background-color: initial;transition: all var(--transitionTime);box-sizing: border-box;font-family: inherit;font-size: inherit;}
 ._svg{display: block;border: 0 solid var(--borderColor);outline: 0px solid var(--outlineColor);margin: 0;padding: 0;transition: all var(--svgTransitionTime);}._svg path,._svg circle,._svg rect,._svg line {transition: all var(--svgTransitionTime);}
-.displayflex{display:flex;}.displaynone{display:none;}.positionrelative{position:relative;}.positionabsolute{position:absolute;}.top6{top:1.5rem;}.right0{right:0rem;}.pb2{padding-bottom:0.5rem;}.pl2{padding-left:0.5rem;}.borderRadius2{border-radius:0.5rem;}.cursorpointer{cursor:pointer;}.aicenter{align-items:center;}.dcolumn{flex-direction:column;}.bgColorwhite{background-color:var(--colorwhite);;}.hoverGroupparent:hover .displayHoverparentblock{display:block;}@media(min-width: 640px){}@media(min-width: 768px){}@media(min-width: 1024px){}@media(min-width: 1280px){}@media(min-width: 1536px){}</style><title>my website</title></head><div class="_box displayflex aicenter positionrelative cursorpointer pb2 pl2 hoverGroupparent"><div class="_box displaynone bgColorwhite positionabsolute right0 top6 borderRadius2 dcolumn displayHoverparentblock"><div class="_box">test</div></div></div></html>`;
+.displayflex{display:flex;}.displaynone{display:none;}.positionrelative{position:relative;}.positionabsolute{position:absolute;}.top6{top:1.5rem;}.right0{right:0rem;}.pb2{padding-bottom:0.5rem;}.pl2{padding-left:0.5rem;}.borderRadius2{border-radius:0.5rem;}.cursorpointer{cursor:pointer;}.aicenter{align-items:center;}.dcolumn{flex-direction:column;}.bgColorwhite{background-color:var(--colorwhite);;}.hoverGroupparent:hover .displayhoverparentblock{display:block;}@media(min-width: 640px){}@media(min-width: 768px){}@media(min-width: 1024px){}@media(min-width: 1280px){}@media(min-width: 1536px){}</style><title>my website</title></head><div class="_box displayflex aicenter positionrelative cursorpointer pb2 pl2 hoverGroupparent"><div class="_box displaynone bgColorwhite positionabsolute right0 top6 borderRadius2 dcolumn displayhoverparentblock"><div class="_box">test</div></div></div></html>`;
 
     expect(result.html).toEqual(expected);
   });
@@ -70,7 +70,7 @@ a,ul{all: unset;}
     const result = renderToStaticMarkup(el);
     const result2 = renderToStaticMarkup(el);
 
-    const expected = '<div class="_box hoverGroupparent"><div class="_box displayHoverparentgrid">test</div></div>';
+    const expected = '<div class="_box hoverGroupparent"><div class="_box displayhoverparentgrid">test</div></div>';
 
     expect(result.html).toEqual(expected);
     expect(result2.html).toEqual(expected);
