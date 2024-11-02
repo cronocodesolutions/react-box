aec91e948d7f11c84491f21a0853a480afe94365
maxpro <maxim.procopenco@gmail.com>
2024-11-02 12:30:16 +0200

    Improve dropdown page

--- pages/pages/dropdownPage.tsx
+++ pages/pages/dropdownPage.tsx
@@ -15,20 +15,21 @@ export default function DropdownPage() {
       </Box>
       <Box py={3}>
         <Dropdown onChange={(value) => console.log(value)}>
-          <Dropdown.NullItem>Select Item</Dropdown.NullItem>
+          <Dropdown.NullItem>Select</Dropdown.NullItem>
           <Dropdown.Item value="test">test</Dropdown.Item>
           <Dropdown.Item value="test2">test2</Dropdown.Item>
         </Dropdown>
       </Box>
       <Box py={3}>
         <Dropdown disabled>
+          <Dropdown.NullItem>Disabled</Dropdown.NullItem>
           <Dropdown.Item value="test">Disabled</Dropdown.Item>
           <Dropdown.Item value="test2">test2</Dropdown.Item>
         </Dropdown>
       </Box>
       <Box py={3}>
         <Dropdown name="item" minWidth={40} isSearchable>
-          <Dropdown.NoItems>Empty</Dropdown.NoItems>
+          <Dropdown.NoItems>No options</Dropdown.NoItems>
           <Dropdown.NullItem>Select</Dropdown.NullItem>
           <Dropdown.Item value={10}>ten</Dropdown.Item>
           <Dropdown.Item value={JSON.stringify({ a: 4, b: 4 })}>twenty</Dropdown.Item>
@@ -48,26 +49,7 @@ export default function DropdownPage() {
       </Box>
       <Box py={3}>
         <Dropdown name="item" minWidth={40} isSearchable>
-          <Dropdown.NoItems>Empty</Dropdown.NoItems>
-          <Dropdown.Item value={10}>ten</Dropdown.Item>
-          <Dropdown.Item value={JSON.stringify({ a: 4, b: 4 })}>twenty</Dropdown.Item>
-          <Dropdown.Item value={30}>thirty</Dropdown.Item>
-          <Dropdown.Item value={301}>thirty2</Dropdown.Item>
-          <Dropdown.Item value={302}>thirty3</Dropdown.Item>
-          <Dropdown.Item value={3043}>thirty4</Dropdown.Item>
-          <Dropdown.Item value={304}>
-            hx
-            <Box inline>
-              test<Box>max</Box>
-              <Box>max2</Box>
-            </Box>
-            <Box inline>test2</Box>
-          </Dropdown.Item>
-        </Dropdown>
-      </Box>
-      <Box py={3}>
-        <Dropdown name="item" minWidth={40} isSearchable>
-          <Dropdown.NoItems>Empty</Dropdown.NoItems>
+          <Dropdown.NoItems>No options</Dropdown.NoItems>
           <Dropdown.Item value={10}>ten</Dropdown.Item>
           <Dropdown.Item value={JSON.stringify({ a: 4, b: 4 })}>twenty</Dropdown.Item>
           <Dropdown.Item value={30}>thirty</Dropdown.Item>
